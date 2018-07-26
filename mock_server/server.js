b20e4cc289f5f68807fb8cbfe395401516007ee3
Stoicescu Sorin <stoicescus@yahoo.com>
2018-07-26 17:01:10 +0300

    Added mockup server in nodejs for simulating real http calls

---
+++ mock_server/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.get('/order_by_id/:id', (req, res) => {
+    let id = req.params.id;
+    let filepath = `./mock_files/order${id}.json`;
+
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.send(JSON.parse(data));
+    })
+});
+
+app.get('/product_by_id/:id', (req, res) => {
+    let id = req.params.id;
+    let filepath = `./mock_files/products.json`;
+    let obj_by_id = null;
+
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        obj_by_id = JSON.parse(data).filter((obj) => {
+            return obj.id === id;
+        });
+
+        res.send(obj_by_id);
+    })
+});
+
+app.get('/customers', (req, res) => {
+    let filepath = `./mock_files/customers.json`;
+
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.send(JSON.parse(data));
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Started mock server on port 3000');
+});
+
+module.exports = {
+    app
+};
